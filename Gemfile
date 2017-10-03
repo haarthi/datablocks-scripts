e89e77da0539019586ed13011c9cc6e023b81f98
haarthi <haarthi@looker.com>
2017-10-03 16:30:33 +0000

    Changes to PROD env

--- Gemfile
+++ Gemfile
@@ -9,7 +9,7 @@ gem 'google-api-client', '~> 0.11.0'
 gem 'aws-sdk'
 
 gem 'json_converter'
-gem "rest-client"
+gem 'rest-client'
 
 gem 'google-cloud-bigquery'
 gem 'google-cloud-storage'
--- exchangerate.sh
+++ exchangerate.sh
@@ -7,4 +7,4 @@ source /usr/share/rvm/environments/jruby-9.0.5.0
 #export JRUBY_OPTS='-J-Xmx4000m -J-Xms4000m -J-XX:+UseG1GC' 
 cd /home/lookerops/datablocks-etl/lib
 
-jruby -J-Xmx2000m -J-Xms2000m  weather.rb
+jruby -J-Xmx2000m -J-Xms2000m  exchangerate.rb
